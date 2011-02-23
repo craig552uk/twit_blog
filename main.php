2b09ff84791a2470de9ea03dd5498401e745b895
Craig Russell <craig@craig-russell.co.uk>
2011-02-23 20:11:19 +0000

    Meta data in main file

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Twit Blog
+Plugin URI: http://www.craig-russell.co.uk
+Description: A plugin to create blog posts form the favourites on a twitter account
+Version: 0.1
+Author: Craig Russell
+Author URI: http://www.craig-russell.co.uk
+License: MIT
+*/
