40e47d1ae4e98a1f9499e0974d8d3b93814e62eb
Craig Russell <craig@craig-russell.co.uk>
2011-02-23 23:43:17 +0000

    Changed double to single quotes for consistency

--- main.php
+++ main.php
@@ -11,7 +11,7 @@ License: MIT
 Copyright (c) 2011 Craig Russell
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
+of this software and associated documentation files (the 'Software'), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
@@ -20,7 +20,7 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -37,7 +37,7 @@ add_action('init','twit_blog');
 
 function twit_blog(){
     if (twit_blog_is_update()) {
-        twit_blog_insert_post("A Sample Message".date('UTC'), "@craig552uk");
+        twit_blog_insert_post('A Sample Message'.date('UTC'), '@craig552uk');
         echo $user_ID;
     }
 }
@@ -47,17 +47,17 @@ register_activation_hook(__FILE__,'twit_blog_install');
 register_deactivation_hook( __FILE__, 'twit_blog_remove' );
 
 function twit_blog_install() {
-    add_option("twit_blog_last_update", date('UTC'), '', 'yes');
-    add_option("twit_blog_update_delay", '10', '', 'yes');
-    add_option("twit_blog_post_author", 'abc', '', 'yes');
-    add_option("twit_blog_post_category", '', '', 'yes');
+    add_option('twit_blog_last_update', date('UTC'), '', 'yes');
+    add_option('twit_blog_update_delay', '10', '', 'yes');
+    add_option('twit_blog_post_author', 'abc', '', 'yes');
+    add_option('twit_blog_post_category', '', '', 'yes');
 }
 
 function twit_blog_remove(){
-    delete_option("twit_blog_last_update");
-    delete_option("twit_blog_update_delay");
-    delete_option("twit_blog_post_author");
-    delete_option("twit_blog_post_category");
+    delete_option('twit_blog_last_update');
+    delete_option('twit_blog_update_delay');
+    delete_option('twit_blog_post_author');
+    delete_option('twit_blog_post_category');
 }
 
 /* Plugin Settings Page */
