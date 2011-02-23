c0ce6a8856874470c5541746f2dce7bfb0cf1d17
Craig Russell <craig@craig-russell.co.uk>
2011-02-23 20:39:03 +0000

    Hello World Example

--- main.php
+++ main.php
@@ -28,3 +28,45 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+
+/* Main Function */
+add_action('init','hello_world');
+
+function hello_world(){
+    echo get_option('hello_world_data');
+}
+
+/* Options */
+register_activation_hook(__FILE__,'hello_world_install');
+register_deactivation_hook( __FILE__, 'hello_world_remove' );
+
+function hello_world_install() {
+    add_option("hello_world_data", 'Default', '', 'yes');
+}
+
+function hello_world_remove(){
+    delete_option("hello_world_data");
+}
+
+/* Settings Page */
+if(is_admin()){
+    add_action('admin_menu','hello_world_admin_menu'); 
+}
+
+function hello_world_admin_menu(){
+    add_options_page('Hello World', 'Hello World', 'administrator', 'hello-world', 'hello_world_html_page');
+}
+
+function hello_world_html_page() { ?>
+    <div>
+        <h2>Hello World Options</h2>
+        <form method="post" action="options.php">
+            <?php wp_nonce_field('update-options'); ?>
+            <input name="hello_world_data" type="text" id="hello_world_data" value="<?php echo get_option('hello_world_data'); ?>" />
+            <input type="hidden" name="action" value="update" />
+            <input type="hidden" name="page_options" value="hello_world_data" />
+            <input type="submit" value="<?php _e('Save Changes') ?>" />
+        </form>
+    </div>
+<?php } ?>
+
