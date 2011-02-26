95ea977623cbbed74875bb9f7fc637417794abd9
Craig Russell <craig@craig-russell.co.uk>
2011-02-26 14:48:40 +0000

    Added oAuth key settings

--- options-page.php
+++ options-page.php
@@ -21,6 +21,30 @@
                 </tr>
               </tbody>
         </table>
+        
+        <h3>OAuth Options</h3>
+        <table class="form-table">
+            <tbody>
+                <tr valign="top">
+                    <th scope="row"><label for="twit_blog_consumer_key">Consumer Key</label></th>
+                    <td>
+                        <input type="text" name="twit_blog_consumer_key" value="<?php echo get_option('twit_blog_consumer_key'); ?>" class="regular-text" />
+                        <span class="description">OAuth Consumer Key from Twitter</span>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row"><label for="twit_blog_consumer_secret">Consumer Secret</label></th>
+                    <td>
+                        <input type="text" name="twit_blog_consumer_secret" value="<?php echo get_option('twit_blog_consumer_secret'); ?>" class="regular-text"/>
+                        <span class="description">OAuth Consumer Secret from Twitter</span>
+                    </td>
+                </tr>
+              </tbody>
+        </table>
+        
         <p class="submit"><input type="submit" id="submit" value="<?php _e('Save Changes') ?>" class="button-primary" /></p>
     </form>
+    
 </div>
+
+
